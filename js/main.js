e6694976793076a4d499dabc88496881727aff1a
nakayamanatsumi <ai.ntmtdr0606@gmail.com>
2025-06-02 23:35:24 +0900

    syuusei

--- js/main.js
+++ js/main.js
@@ -12,18 +12,24 @@ $(document).ready(function () {
     $(".header__nav-item , .request-btn").click(function (e) {
         const targetId = $(this).attr("href");
         const target = $(targetId);
+        const headerHeight = $(".header").outerHeight();
 
         if (target.length) {
             e.preventDefault(); 
 
             $("html, body").animate({
-                scrollTop: target.offset().top
+                scrollTop: target.offset().top - headerHeight
             }, 600);
 
             $(".menu-trigger").removeClass("active");
             $(".header__nav, .header__nav-list, .header__nav-item, .hamburger-only-list, body").removeClass("active");
         }
+
+
+        
+        
     });
+
 
     /* スクロールでfade-in */
     $(window).on("scroll", function() {
@@ -54,6 +60,73 @@ $(document).ready(function () {
             $question.addClass('active');
         }
     });
+
+    /* form */
+    const $form = $(".downloadForm");
+    const $inputs = $form.find("input[required]");
+    const $submitBtn = $form.find(".downloadBtn");
+
+    $submitBtn.prop("disabled", true);
+
+    $inputs.on("input", function () {
+        let allFilled = true;
+
+        $inputs.each(function () {
+            if ($(this).val().trim() === "") {
+                allFilled = false;
+            }
+        });
+
+        $submitBtn.prop("disabled", !allFilled); 
+    });
+
+    $form.on("submit", function (e) {
+        e.preventDefault();
+        $submitBtn.hide();
+        $form.find(".thankYouMessage").fadeIn();
+    });
+
+    // achievements
+const autoSwiper = new Swiper(".Achievements__swiper", {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 80,
+    speed: 6000,
+    allowTouchMove: false,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+
+    breakpoints: {
+        951: {
+            spaceBetween: 63,
+        },
+    }
+});
+
+// case
+const caseSwiper = new Swiper('.case__swiper', {
+    
+    slidesPerGroup: 1,
+    slidesPerView: 1,
+    allowTouchMove: false,
+
+    spaceBetween: 48,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        951: {
+            slidesPerGroup: 2,
+            slidesPerView: 2,
+            spaceBetween: 70,
+        },
+    }
 });
 
 
+});
